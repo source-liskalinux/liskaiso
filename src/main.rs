c5ff16f6d77be70ceb06df92ca8431b751168843
janorovicvolkov <janorovicvolkov@gmail.com>
2026-07-28 04:57:36 +0200

    Update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -213,7 +213,7 @@ const CLI_EDITION: Edition = Edition {
         "nano", "vim", "grub", "libverto", "wget", "curl", "git", "which", "man-db",
         "man-pages", "lkinit", "util-linux", "coreutils", "findutils", "sed", "grep",
         "kmod", "e2fsprogs", "iputils", "gptfdisk", "parted", "dosfstools", "btrfs-progs",
-        "xfsprogs", "ca-certificates",
+        "xfsprogs", "ca-certificates", "nghttp3", "nghttp2", "openssl",
     ],
 };
 
